3fc134b1e37b7fbb88488301085cff37de4a86a1
steven7kramer <steven7kramer@gmail.com>
2018-11-19 08:55:57 +0100

    afronding infoInfowindow zonder losse paginas

--- js/BackseatInfo.js
+++ js/BackseatInfo.js
@@ -17,20 +17,20 @@ function getURLParameter(sParam)
 }
 
 var infoUrlLoader = ['valkenburg','brittenburg'];
-var infoHeaderBG = ['landen.jpg','placeholder.jpg'];
+var infoHeaderBG = ['landen.jpg','brittenburg.jpg'];
 var titleArray = [
 ['Hoi!','Tweede Wereldoorlog','Toch proberen te landen...','Omleiding','Wat is er nu?'],
-['Titel Placeholder','Titel Placeholder','Titel Placeholder','Titel Placeholder','Titel Placeholder'],
+['Hoi!','De Limes','Onderzoek','De Restanten'],
 ];
 
 var imageArray = [
 ['sofieTulp2.png','wo2.jpg','landen2.jpg','vliegtuigen.jpg','soldaatoranje.jpg'],
-['placeholder.jpg','placeholder.jpg','placeholder.jpg','placeholder.jpg','placeholder.jpg'],
+['sofieTulp2.png','limes.png','tekening.jpg','resten.jpg'],
 ];
 
 var descrArray = [
 ['Welkom bij Vliegveld Valkenburg. Dit kleine tweebaans vliegveld kent een bijzondere geschiedenis na de bouw in 1939. Swipe naar links om deze te ontdekken!','Tijdens de Tweede Wereldoorlog is dit vliegveld in handen gevallen van de Duitsers. Dit vliegveld was alleen helemaal nog niet voltooid! Hier waren de Duitsers niet van op de hoogte en gingen dit vliegveld alsnog gebruiken voor transport van mensen en materialen...','Het landen van de zware vliegtuigen op de nog onvoltooide landingsbaan ging niet volgens plan. Doordat de grond te zacht was strandde het eerste vliegtuig al halverwege de landingsbaan. Het tweede vliegtuig volgde door een druk tijdschema al snel, zonder te weten dat er nog een vliegtuig vast stond op de landingsbaan. Deze botsten dus op grote snelheid, evenals een aantal van de volgende vliegtuigen. Dit ging niet helemaal lekker dus!','Toen de Duitsers er eindelijk achterkwamen dat de landingsbaan nog niet geschikt was om op te landen, besloten ze de missie nog niet af te lasten. Het was namelijk erg belangrijk voor ze om snel het kabinet in Den Haag gevangen te nemen om zo de belangrijkste invloeden binnen Nederland onschadelijk te maken. Daarom maakten de aankomende vliegtuigen een noodlanding op het strand van Scheveningen.','Het vliegveld is tijdens en na de oorlog veelvuldig gebruikt. Momenteel zijn de landingsbanen al sinds 2010 buiten gebruik en wordt een hangar gebruikt voor een musical. Vanaf 2020 zal dit vliegveld verdwijnen en zullen er woonwijken voor in de plaats komen.'],
-['Description Placeholder','Description Placeholder','Description Placeholder','Description Placeholder','Description Placeholder'],
+['Welkom bij Brittenburg, de Romeinse ruïne van een limesvesting. Swipe naar links om hier meer over te weten te komen!','De limes (Latijn voor "grens") is de aanduiding van de grens en verdedigingszone van het Romeinse Rijk, hoofdzakelijk gebouwd in de periode 40 na Chr - ca 250 na Chr. Deze liep van de atlantische kust in Noord- Engeland via de Noordzee langs de toenmalige hoofdstroom van de Rijn en Donau naar de Zwarte Zee.','Recent archeologisch onderzoek op de plaats waar de Brittenburg volgens recente gegevens kan liggen heeft niets opgeleverd. Wel zijn in 1982 bij het uitgraven van de huidige Uitwateringssluizen duidelijke aanwijzingen gevonden voor een Romeinse nederzetting, die mogelijk verband houdt met Lugdunum Batavorum.','De resten van dit castellum liggen tegenwoordig, door het terugwijken van de kustlijn, in zee. Nog tot in de twintigste eeuw zouden er bij extreem laag water resten van dit castellum (vanaf de zestiende eeuw de Brittenburg genoemd) te zien zijn geweest. De zee heeft waarschijnlijk de laatste resten weggespoeld, en wie tegenwoordig over het strand van Noordwijk naar de uitwatering bij Katwijk loopt zal zich moeilijk kunnen voorstellen dat daar ergens in zee ooit het eindpunt lag van de Romeinse limes, het imposante castellum Lugdunum.'],
 ];
 
 var infoContainer = document.getElementById('infoContainer');
@@ -77,11 +77,12 @@ function showInfo(){
 			// add this question and its answers to the output
       // titleArray is used, but it could have been any array, it's only the length that matters
       output.push('<div class="responsive contentHeight">');
+      var imageUrl = "'../../images/POI/infowindow" + (infoToLoad + 1) + '/' + infoHeaderBG[infoToLoad] + "'";
 
       for(let l=0; l<titleArray[infoToLoad].length; l++){
         output.push(
           '<div class="slickContent">'
-          +  '<div class="slickBgInfo">'
+          +  '<div class="slickBgInfo" style="background-image:url(' + imageUrl + ')">'
           +    '<img src="../../images/POI/infowindow' + (infoToLoad + 1) + '/' + headerImg[l] + '" />'
           +  '</div>'
           +  '<div class="slickText">'
