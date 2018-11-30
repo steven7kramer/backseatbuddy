89cab5dccf4c62900b10b56e12a899899aba59c5
steven7kramer <steven7kramer@gmail.com>
2018-11-30 15:27:47 +0100

    fixed every change since git mistakes

--- pages/POI/windmolens.php
+++ pages/POI/windmolens.php
@@ -10,6 +10,7 @@
 
         <!-- CSS -->
         <link rel="stylesheet" href="../../../css/bsb_style.css">
+        <link rel="stylesheet" href="../../../css/windmolens.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
         <!-- Libraries -->
